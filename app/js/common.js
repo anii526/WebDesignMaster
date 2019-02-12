64f583d60ba375e9c0f8e13256facc9a19dd16f5
anii526 <ani526dev@gmail.com>
2019-02-12 23:59:52 +0300

    меню сайта готово

--- app/js/common.js
+++ app/js/common.js
@@ -5,4 +5,27 @@ $(function () {
 		ths.html(ths.html().replace('O', '<span>O</span>'))
 	});
 
+	$('.search').click(function () {
+		$('.search-field').stop().slideToggle();
+		$('.search-field input[type=text]').focus();
+	});
+
+	$(document).keyup(function (e) {
+		if (e.keyCode == 27) {
+			$('.search-field').stop().slideUp();
+		}
+	}).click(function () {
+		$('.search-field').stop().slideUp();
+	});
+
+	$('.search-wrap').click(function (e) {
+		e.stopPropagation();
+	});
+
+	$('.top-line').after('<div class="mobile-menu d-lg-none">');
+	$('.top-menu').clone().appendTo('.mobile-menu');
+	$('.mobile-menu-button').click(function () {
+		$('.mobile-menu').stop().slideToggle();
+	});
+
 });
